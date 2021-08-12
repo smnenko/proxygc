fd4f69a1ae6ac3aab9dc5d2ae9bca25688b39321
Stanislav <stanichgame@gmail.com>
2021-08-12 15:09:36 +0300

    program can parse last page

--- proxy/grabber.py
+++ proxy/grabber.py
@@ -1,2 +1,39 @@
+import json
+import asyncio
+import re
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from .config import Color
+
+
 class ProxyGrabber:
-    pass
+    sources = json.load(open(Path().absolute().joinpath('proxy').joinpath('sources.json'), mode='r', encoding='utf-8'))
+    # driver = webdriver.Chrome()
+
+    def __init__(self):
+        print(Color.FAIL + 'ProxyGrabber initialized' + Color.ENDC)
+
+    def _check_is_available(self, url):
+        return requests.get(url)
+
+    def _get_last_page(self, html, page):
+        soup = BeautifulSoup(html, 'lxml')
+        pages = soup.find(page['block'], id=page['id'], class_=page['class']).find_all(page['inside'])
+        return [i.text for i in pages if i.text.isdigit()][-1]
+
+    def start(self):
+        print(f'Available {len(self.sources)} sites in base')
+        print('Starting...\n')
+        for key, value in self.sources.items():
+            print(f"GRAB | {value['name']}({key})", end=' ')
+            response = self._check_is_available(value['url'])
+            if response.status_code == 200:
+                print(Color.OKGREEN + '[OK]' + Color.ENDC, end=' ')
+                last_page = self._get_last_page(response.content, value['page'])
+                print(f"PAGES: {last_page}", end=' ')
+            else:
+                print('[ERROR]')
---
+++ proxy/manager.py
@@ -0,0 +1,32 @@
+from .grabber import ProxyGrabber
+from .checker import ProxyChecker
+
+AVAILABLE_OPTIONS = {
+    1: {
+        'option': 'Grab proxies from our list of sites',
+        'class': ProxyGrabber
+    },
+    2: {
+        'option': 'Check proxies for valid',
+        'class': ProxyChecker
+    }
+}
+
+
+class ProxyManager:
+
+    def __init__(self):
+        self.choice = None
+
+    def set_choice(self, choice):
+        self.choice = choice
+
+    def run(self):
+        if self.choice.isdigit() and int(self.choice) in AVAILABLE_OPTIONS:
+            return AVAILABLE_OPTIONS[int(self.choice)]['class']().start()
+        raise Exception('Invalid input')
+
+    @classmethod
+    def available_options(cls):
+        for key, value in AVAILABLE_OPTIONS.items():
+            print(f"{key}: {value['option']}")
